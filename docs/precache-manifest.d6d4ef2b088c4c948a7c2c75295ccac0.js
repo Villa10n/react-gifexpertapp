6e825ea630241d3b17388cb2612270c5e4cd737a
villa <joaquin.villalon@mail.udp.cl>
2020-09-16 13:15:51 -0300

    docs agregado

---
+++ docs/precache-manifest.d6d4ef2b088c4c948a7c2c75295ccac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a825d71ba62b3f37c042bd5ff7b16edf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c34eab8b636e10b12455",
+    "url": "/static/css/main.f10b3e6b.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c34eab8b636e10b12455",
+    "url": "/static/js/main.85ef3e48.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
